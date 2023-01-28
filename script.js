e429299d4d6e34d3b6f01fe81eb2a479060e8cb9
Galih Roswandi <galihroswandi25@gmail.com>
2023-01-28 22:36:46 +0700

    Custom Element WEB_COMPONENT

---
+++ script.js
@@ -0,0 +1,49 @@
+// Custom Element HTML
+
+class MyElement extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = 'Hello World';
+    }
+}
+
+customElements.define('my-element', MyElement);
+
+
+class Author extends HTMLElement {
+
+    // constructor() {
+    //     super();
+
+    //     // create a shadow dom root
+    //     this.attachShadow({ mode: 'open' });
+
+    //     // add some CSS to the shadow root
+    //     this.shadowRoot.innerHTML = `
+    //         <style>
+    //             h1{
+    //                 color: blue;
+    //             }
+
+    //             p{
+    //                 font-size: 14px;
+    //             }
+    //         </style>
+    //     `;
+    // }
+
+    // connectedCallback() {
+    //     this.shadowRoot.innerHTML += `
+    //         <h1>Nama Saya Galih Roswandi</h1>
+    //         <p>Tasikmalaya</p>
+    //     //     `;
+    // }
+
+    connectedCallback() {
+        this.innerHTML = `
+            <h1>Nama Saya Galih Roswandi</h1>
+            <p>Tasikmalaya</p>
+        `;
+    }
+}
+
+customElements.define('my-author', Author);
